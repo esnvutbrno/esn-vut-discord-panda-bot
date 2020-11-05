767d56fec895e76d94486358b5c1da173242adfe
Josef Kolář <thejoeejoee@gmail.com>
2020-11-05 22:24:57 +0100

    Added working welcome message

--- bot/commands/welcome.py
+++ bot/commands/welcome.py
@@ -1,8 +1,23 @@
-from discord import Member
+import discord
+from discord import Member, Guild, TextChannel
 
-from . import bot
+from . import bot, Optional
+from ..conf import PANDA_EMOJI
 
 
 @bot.event
 async def on_member_join(member: Member):
-    pass # await member.send('Hello, I am panda!\nI\'m ')
+    """
+    Sends welcome message for new user to general channel.
+    """
+    if not member.guild or not member.guild.system_channel:
+        return
+
+    g: Guild = member.guild
+
+    rules: Optional[TextChannel] = discord.utils.get(g.channels, name='rules')
+
+    await g.system_channel.send(
+        f'Cheers {member.mention}, **Panda from ESN VUT Brno** welcomes you here! {PANDA_EMOJI}\n'
+        f'*Shhh, I recommend you to see the {rules.mention if rules else "#rules"} channel.*'
+    )
