b1c15b45a997d5ec56fbea22861c3868e8afb2e0
Josef Kolář <thejoeejoee@gmail.com>
2021-02-19 18:11:56 +0100

    Better welcome message II

--- bot/commands/welcome.py
+++ bot/commands/welcome.py
@@ -15,7 +15,7 @@ async def on_member_join(member: Member):
     g: Guild = member.guild
 
     await g.system_channel.send(
-        f'Cheers {member.mention}, **Panda welcomes you on {g.name} Discord! {PANDA_EMOJI}\n'
+        f'Cheers {member.mention}, **{bot.user.display_name} welcomes you on {g.name} Discord! {PANDA_EMOJI}\n'
         f'*Shhh, the panel of the left offers plenty of channels of various quality, and level of fun - feel free to '
         f'explore them!*'
     )
