8346068e627618c728cccfb08f59cdc9c36e1515
Josef Kolář <thejoeejoee@gmail.com>
2021-02-19 18:21:38 +0100

    Better welcome message III

--- bot/commands/welcome.py
+++ bot/commands/welcome.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from discord import Member, Guild
 
 from . import bot
@@ -14,8 +16,11 @@ async def on_member_join(member: Member):
 
     g: Guild = member.guild
 
+    m: Optional[Member] = g.get_member(user_id=bot.user.id)
+
     await g.system_channel.send(
-        f'Cheers {member.mention}, **{bot.user.display_name} welcomes you on {g.name} Discord! {PANDA_EMOJI}\n'
+        f'Cheers {member.mention}, **{m.display_name if m else bot.user.display_name}**'
+        f'welcomes you on {g.name} Discord! {PANDA_EMOJI}\n'
         f'*Shhh, the panel of the left offers plenty of channels of various quality, and level of fun - feel free to '
         f'explore them!*'
     )
